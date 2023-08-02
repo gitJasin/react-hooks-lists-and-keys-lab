15ba67a60295a57cd0d5dd0e6ef5d46b5be7169b
Jason Stewart <jaysin.stewart@gmail.com>
2023-08-02 11:25:05 -0700

    completed lists-n-keys lab

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,17 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  return <nav>{links.map(link => <a key={link} href={`#${link}`}>{link}</a>)}</nav>;
 }
 
 export default NavBar;
+
+// [X] - In the `NavBar` component, there is an array with three strings representing
+// each link on the page. For each of those strings, create an `<a>` tag that looks
+// like this:
+
+// ```jsx
+// <a href="#home">home</a>
+// ```
+
+// Make sure each `<a>` element also gets a unique `key` prop. 
--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -3,12 +3,23 @@ import ProjectItem from "./ProjectItem";
 
 function ProjectList({ projects }) {
   console.log(projects);
+
+   const projectObjects = projects.map(project => <ProjectItem key={project.id} name={project.name} about={project.about} technologies={project.technologies}/>)
+
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">
+        {projectObjects}
+        {projects.technologies}
+      </div>
     </div>
   );
 }
 
 export default ProjectList;
+
+// [X] - In the `ProjectList` component, a prop of `projects` is being passed down from
+// the `App` component with an array of objects. For each object in the array,
+// render one `ProjectItem` component with the correct props. Use the `id` of the
+// project for the `key` prop.
